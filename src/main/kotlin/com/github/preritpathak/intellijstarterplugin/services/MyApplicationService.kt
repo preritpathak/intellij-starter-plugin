aab13078e574fc8a1f756bfaa8787abe79e12805
GitHub Action <action@github.com>
2022-11-08 06:34:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/preritpathak/intellijstarterplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/preritpathak/intellijstarterplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.preritpathak.intellijstarterplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.preritpathak.intellijstarterplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/preritpathak/intellijstarterplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/preritpathak/intellijstarterplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.preritpathak.intellijstarterplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.preritpathak.intellijstarterplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
